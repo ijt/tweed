603f9d01d209f4741c394cc9c9876dd302ec1ee6
Issac Trotts <issac.trotts@gmail.com>
2019-10-31 22:22:37 -0700

    show only the text of the tweets, take keywords as cmd line arg

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
+use serde::{Deserialize, Serialize};
 use std::env;
+use std::process::exit;
 use twitter_stream::rt::{self, Future, Stream};
 use twitter_stream::{Token, TwitterStreamBuilder};
 
@@ -10,16 +12,38 @@ fn main() {
         env::var("ACCESS_SECRET").unwrap(),
     );
 
+    let args: Vec<String> = env::args().collect();
+    if args.len() != 1 + 1 {
+        println!(
+            "usage: tweed keywords
+
+where keywords is a comma-separated list of topic keywords
+"
+        );
+        exit(1);
+    }
+    let keywords: &str = &args[1].to_string();
+
     let future = TwitterStreamBuilder::filter(token)
-        .track(Some("@Twitter"))
+        .track(Some(keywords))
         .listen()
         .unwrap()
         .flatten_stream()
         .for_each(|json| {
-            println!("{}", json);
+            let tr: serde_json::Result<Tweet> = serde_json::from_str(&json.to_string());
+            match tr {
+                Err(_e) => (),
+                Ok(t) => println!("{}\n", t.text),
+            }
             Ok(())
         })
         .map_err(|e| println!("error: {}", e));
 
     rt::run(future);
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+struct Tweet {
+    created_at: String,
+    text: String,
 }
